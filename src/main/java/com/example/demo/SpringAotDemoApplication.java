c3a899ebf1ed06045e72e33c4ee642918ef5e10b
oanhpv <oanhmrphamvan@gmail.com>
2026-06-20 11:25:54 +0700

    demo swwager 4

---
+++ src/main/java/com/example/demo/SpringAotDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAotDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringAotDemoApplication.class, args);
+	}
+}
